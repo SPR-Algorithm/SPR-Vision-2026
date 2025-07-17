06350ff2154400316cd5d6161ae65861a0841c79
DA_WANG\35031 <3503194162@qq.com>
2025-07-17 14:09:35 +0800

    添加相机参数配置文件，更新启动参数以支持新相机节点

--- Main_ws/src/rm_bringup/launch/bringup.launch.py
+++ Main_ws/src/rm_bringup/launch/bringup.launch.py
@@ -1,22 +1,23 @@
 import os
 import sys
 import yaml
+from launch.actions import RegisterEventHandler, Shutdown, LogInfo
+from launch.event_handlers import OnProcessExit
 from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import Command
 sys.path.append(os.path.join(get_package_share_directory('rm_bringup'), 'launch'))
 
-
 def generate_launch_description():
-
+    from launch_ros.actions import Node, SetParameter, PushRosNamespace
+    from launch.actions import TimerAction
     from launch_ros.descriptions import ComposableNode
-    from launch_ros.actions import ComposableNodeContainer, Node, SetParameter, PushRosNamespace
-    from launch.actions import TimerAction, Shutdown
     from launch import LaunchDescription
 
     launch_params = yaml.safe_load(open(os.path.join(
         get_package_share_directory('rm_bringup'), 'config', 'launch_params.yaml')))
 
-    SetParameter(name='rune',value=launch_params['rune']),
+    # 初始化参数和机器人描述（保持原样）
+    SetParameter(name='rune', value=launch_params['rune']),
     robot_gimbal_description = Command(['xacro ', os.path.join(
         get_package_share_directory('rm_robot_description'), 'urdf', 'rm_gimbal.urdf.xacro'),
         ' xyz:=', launch_params['odom2camera']['xyz'], ' rpy:=', launch_params['odom2camera']['rpy']])
@@ -34,33 +35,63 @@ def generate_launch_description():
     robot_navigation_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_navigation_description,}]
-                    # 'publish_frequency': 1000.0}]
+        parameters=[{'robot_description': robot_navigation_description}]
     )
 
     def get_params(name):
-        return os.path.join(get_package_share_directory('rm_bringup'), 'config', 'node_params', '{}_params.yaml'.format(name))
+        return os.path.join(get_package_share_directory('rm_bringup'), 'config', 'node_params', f'{name}_params.yaml')
 
-    # 图像
-    if launch_params['video_play']: 
-        image_node  = ComposableNode(
-            package='rm_camera_driver',
-            plugin='fyt::camera_driver::VideoPlayerNode',
-            name='video_player',
-            parameters=[get_params('video_player')],
-            extra_arguments=[{'use_intra_process_comms': True}]
-        )
+    # ================== 修改部分开始 ================== #
+    # 摄像头节点（转换为常规节点）
+    if launch_params['mode']:
+        node_params = os.path.join(
+    get_package_share_directory('rm_bringup'), 'config', 'camera_params_hik.yaml')
     else:
-         image_node  = ComposableNode(
-            package='rm_camera_driver',
-            plugin='fyt::camera_driver::DahengCameraNode',
-            name='camera_driver',
-            parameters=[get_params('camera_driver')],
+        node_params = os.path.join(
+    get_package_share_directory('rm_bringup'), 'config', 'camera_params_mv.yaml')
+    def get_camera_node(package, plugin):
+        return ComposableNode(
+            package=package,
+            plugin=plugin,
+            name='camera_node',
+            parameters=[node_params],
             extra_arguments=[{'use_intra_process_comms': True}]
         )
+    if launch_params['mode']:
+        image_node = get_camera_node('hik_camera', 'hik_camera::HikCameraNode')
+    else:
+        image_node = get_camera_node('mindvision_camera', 'mindvision_camera::MVCameraNode')
 
+    # 装甲板检测节点（转换为常规节点）
+    
+    if launch_params['camera']:
+        armor_detector_node = ComposableNode(
+        package='armor_detector', 
+        plugin='spr::auto_aim::ArmorDetectorNode',
+        name='armor_detector',
+        parameters=[get_params('armor_detector')],
+        extra_arguments=[{'use_intra_process_comms': True}]
+    )
+    else:
+        armor_detector_node = ComposableNode(
+        package='openvino_armor_detector', 
+        plugin='rm_auto_aim::OpenVINODetectNode',
+        name='openvino_armor_detector',
+        extra_arguments=[{'use_intra_process_comms': True}]
+    )
 
-    # 串口
+    # 符石检测节点（转换为常规节点）
+    rune_detector_node = Node(
+        package='rune_detector',
+        executable='rune_detector_node',
+        name='rune_detector',
+        parameters=[get_params('rune_detector')],
+        output='both',
+        emulate_tty=True,
+    )
+    # ================== 修改部分结束 ================== #
+
+    # 串口节点（保持原样）
     if launch_params['virtual_serial']:
         serial_driver_node = Node(
             package='rm_serial_driver',
@@ -79,19 +110,9 @@ def generate_launch_description():
             output='both',
             emulate_tty=True,
             parameters=[get_params('serial_driver')],
-            ros_arguments=['--ros-args', ],
         )
-        
-    # 装甲板识别
-    armor_detector_node = ComposableNode(
-        package='armor_detector', 
-        plugin='fyt::auto_aim::ArmorDetectorNode',
-        name='armor_detector',
-        parameters=[get_params('armor_detector')],
-        extra_arguments=[{'use_intra_process_comms': True}]
-    )
-    
-    # 装甲板解算
+
+    # 装甲板解算节点（保持原样）
     if launch_params['hero_solver']:
         armor_solver_node = Node(
             package='hero_armor_solver',
@@ -100,7 +121,6 @@ def generate_launch_description():
             output='both',
             emulate_tty=True,
             parameters=[get_params('armor_solver')],
-            ros_arguments=[],
         )
     else:
         armor_solver_node = Node(
@@ -110,17 +130,9 @@ def generate_launch_description():
             output='both',
             emulate_tty=True,
             parameters=[get_params('armor_solver')],
-            ros_arguments=[],
         )
 
-    # 打符
-    rune_detector_node = ComposableNode(    
-        package='rune_detector',
-        plugin='fyt::rune::RuneDetectorNode',
-        name='rune_detector',
-        parameters=[get_params('rune_detector')],
-        extra_arguments=[{'use_intra_process_comms': True}]
-        )
+    # 符石解算节点（保持原样）
     rune_solver_node = Node(
         package='rune_solver',
         executable='rune_solver_node',
@@ -128,67 +140,43 @@ def generate_launch_description():
         output='both',
         emulate_tty=True,
         parameters=[get_params('rune_solver')],
-        arguments=['--ros-args',], 
-        )
-
-    # 使用intra cmmunication提高图像的传输速度
-    def get_camera_detector_container(*detector_nodes):
-        nodes_list = list(detector_nodes)
-        nodes_list.append(image_node)
-        container = ComposableNodeContainer(
-            name='camera_detector_container',
-            namespace='',
-            package='rclcpp_components',
-            executable='component_container_mt',
-            composable_node_descriptions=nodes_list,
-            output='both',
-            emulate_tty=True,
-            ros_arguments=['--ros-args', ],
-        )
-        return TimerAction(
-            period=2.0,
-            actions=[container],
-        )
-
-    # 延迟启动
-    delay_serial_node = TimerAction(
-        period=1.5,
-        actions=[serial_driver_node],
     )
 
-    delay_armor_solver_node = TimerAction(
-        period=2.0,
-        actions=[armor_solver_node],
-    )
-    
-    delay_rune_solver_node = TimerAction(
-        period=2.0,
-        actions=[rune_solver_node],
-    )
-    
-    if launch_params['rune']:
-        cam_detector_node = get_camera_detector_container(armor_detector_node, rune_detector_node)
-    else:
-        cam_detector_node = get_camera_detector_container(armor_detector_node)
-
-    delay_cam_detector_node = TimerAction(
-        period=2.0,
-        actions=[cam_detector_node],
-        ) 
-    
+    # ================== 新的启动顺序 ================== #
     push_namespace = PushRosNamespace(launch_params['namespace'])
-    
+
+    # 延迟启动配置
     launch_description_list = [
         robot_gimbal_publisher,
         push_namespace,
-        delay_serial_node,
-        delay_cam_detector_node,
-        delay_armor_solver_node]
-    
+        TimerAction(  # 摄像头节点最先启动
+            period=1.0,
+            actions=[image_node]
+        ),
+        TimerAction(  # 检测节点延迟2秒
+            period=2.0,
+            actions=[armor_detector_node]
+        ),
+        TimerAction(  # 其他节点延迟3秒
+            period=3.0,
+            actions=[serial_driver_node, armor_solver_node]
+        )
+    ]
+
+    # 符石相关节点
     if launch_params['rune']:
-        launch_description_list.append(delay_rune_solver_node)
-    
+        launch_description_list += [
+            TimerAction(
+                period=2.5,
+                actions=[rune_detector_node]
+            ),
+            TimerAction(
+                period=3.5,
+                actions=[rune_solver_node]
+            )
+        ]
+
     if launch_params['navigation']:
         launch_description_list.append(robot_navigation_publisher)
-    
+
     return LaunchDescription(launch_description_list)
