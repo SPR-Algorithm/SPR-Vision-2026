3c19e7c8d85c656f497fbdf002d59cec8715f08e
明瑾 <2517630545@qq.com>
2025-09-27 13:28:03 +0800

    Update OpenVINO version from 2024 to 2023

--- install_and_configure.sh
+++ install_and_configure.sh
@@ -90,9 +90,9 @@ echo "[udev规则添加完成]"
 echo "[开始安装OpenVINO]"
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-echo "deb https://apt.repos.intel.com/openvino/2024 ubuntu22 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2024.list
+echo "deb https://apt.repos.intel.com/openvino/2023 ubuntu22 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2023.list
 sudo apt update
-sudo apt install openvino-2024.6.0
+sudo apt install -y openvino-2023.3.0
 cd neo
 # wget https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17384.11/intel-igc-core_1.0.17384.11_amd64.deb
 # wget https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17384.11/intel-igc-opencl_1.0.17384.11_amd64.deb
